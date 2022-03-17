43470ecad6d6042d33f2df5206baceed9af1aa1e
Pavel Afanasyev <afanasyevpv9@gmail.com>
2022-03-17 19:00:15 +0300

    create a project of the 1st HoneWork

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/Netology_IB_InstrumentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netology_IB_InstrumentsApp.swift
+//  Netology_IB_Instruments
+//
+//  Created by Павел Афанасьев on 17.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct Netology_IB_InstrumentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
